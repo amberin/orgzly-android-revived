b12aa82720be2e8f7024842a33318d40df8c0c38
Victor Andreasson <amberin@users.noreply.github.com>
2025-10-17 21:39:10 +0200

    test

--- app/src/androidTest/java/com/orgzly/android/espresso/util/EspressoUtils.java
+++ app/src/androidTest/java/com/orgzly/android/espresso/util/EspressoUtils.java
@@ -190,7 +190,7 @@ public class EspressoUtils {
 
     public static ViewInteraction onRecyclerViewItem(@IdRes int recyclerView, int position, @IdRes int childView) {
         // Ensure UI is stable before searching for RecyclerView
-        onView(isRoot()).perform(waitForStableRoot());
+        Thread.sleep(500);
         onView(isRoot()).perform(waitId(recyclerView, 5000));
         onView(withId(recyclerView)).perform(RecyclerViewActions.scrollToPosition(position));
         return onView(new EspressoRecyclerViewMatcher(recyclerView)
